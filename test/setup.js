1a2cb44b2b307caf4eca77d9ef7305fabeb14bda
Dhanit Paija <dhanit.paija@gmail.com>
2018-06-06 15:47:00 +0545

    Remove unnecessary settings

--- test/setup.js
+++ test/setup.js
@@ -3,13 +3,3 @@ import Adapter from 'enzyme-adapter-react-15';
 import { configure } from 'enzyme';
 
 configure({ adapter: new Adapter() });
-
-
-// Prevent Mocha from compiling resources like css, scss or svg
-function disableOnTest() {
-  return null;
-}
-
-require.extensions['.css'] = disableOnTest;
-require.extensions['.scss'] = disableOnTest;
-require.extensions['.svg'] = disableOnTest;
